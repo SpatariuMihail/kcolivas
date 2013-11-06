522b1420f8d5f11c3a129c8eb81e033b0d2045a5
ckolivas <kernel@kolivas.org>
2013-11-06 12:26:58 +1100

    Revert control transfers to libusb sync transfers.

--- usbutils.c
+++ usbutils.c
@@ -2789,43 +2789,6 @@ out_noerrmsg:
 	return err;
 }
 
-/* As we do for bulk reads, emulate a sync function for control transfers using
- * our own timeouts that takes the same parameters as libusb_control_transfer.
- */
-static int usb_control_transfer(struct cgpu_info *cgpu, libusb_device_handle *dev_handle, uint8_t bmRequestType,
-				uint8_t bRequest, uint16_t wValue, uint16_t wIndex,
-				unsigned char *buffer, uint16_t wLength, unsigned int timeout)
-{
-	struct usb_transfer ut;
-	unsigned char buf[70];
-	int err, transferred;
-
-	if (unlikely(cgpu->shutdown))
-		return libusb_control_transfer(dev_handle, bmRequestType, bRequest, wValue, wIndex, buffer, wLength, timeout);
-
-	init_usb_transfer(&ut);
-	libusb_fill_control_setup(buf, bmRequestType, bRequest, wValue,
-				  wIndex, wLength);
-	if ((bmRequestType & LIBUSB_ENDPOINT_DIR_MASK) == LIBUSB_ENDPOINT_OUT)
-		memcpy(buf + LIBUSB_CONTROL_SETUP_SIZE, buffer, wLength);
-	libusb_fill_control_transfer(ut.transfer, dev_handle, buf, transfer_callback,
-				     &ut, 0);
-	err = usb_submit_transfer(&ut, ut.transfer, false);
-	if (!err)
-		err = callback_wait(&ut, &transferred, timeout);
-	if (err == LIBUSB_SUCCESS && transferred) {
-		if ((bmRequestType & LIBUSB_ENDPOINT_DIR_MASK) == LIBUSB_ENDPOINT_IN)
-			memcpy(buffer, libusb_control_transfer_get_data(ut.transfer),
-			       transferred);
-		err = transferred;
-		goto out;
-	}
-	err = usb_transfer_toerr(err);
-out:
-	complete_usb_transfer(&ut);
-	return err;
-}
-
 int __usb_transfer(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bRequest, uint16_t wValue, uint16_t wIndex, uint32_t *data, int siz, unsigned int timeout, __maybe_unused enum usb_cmds cmd)
 {
 	struct cg_usb_device *usbdev;
@@ -2861,8 +2824,8 @@ int __usb_transfer(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bReques
 	USBDEBUG("USB debug: @_usb_transfer() buf=%s", bin2hex(buf, (size_t)siz));
 
 	STATS_TIMEVAL(&tv_start);
-	err = usb_control_transfer(cgpu, usbdev->handle, request_type, bRequest,
-				   wValue, wIndex, buf, (uint16_t)siz, timeout);
+	err = libusb_control_transfer(usbdev->handle, request_type,
+		bRequest, wValue, wIndex, buf, (uint16_t)siz, timeout);
 	STATS_TIMEVAL(&tv_finish);
 	USB_STATS(cgpu, &tv_start, &tv_finish, err, MODE_CTRL_WRITE, cmd, SEQ0, timeout);
 
@@ -2874,6 +2837,7 @@ int __usb_transfer(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bReques
 		applog(LOG_WARNING, "%s %i usb transfer err:(%d) %s", cgpu->drv->name, cgpu->device_id,
 		       err, libusb_error_name(err));
 	}
+
 out_:
 	return err;
 }
@@ -2889,9 +2853,6 @@ int _usb_transfer(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bRequest
 	DEVWLOCK(cgpu, pstate);
 
 	err = __usb_transfer(cgpu, request_type, bRequest, wValue, wIndex, data, siz, timeout, cmd);
-
-	if (NOCONTROLDEV(err))
-		release_cgpu(cgpu);
 
 	DEVWUNLOCK(cgpu, pstate);
 
@@ -2925,8 +2886,9 @@ int _usb_transfer_read(struct cgpu_info *cgpu, uint8_t request_type, uint8_t bRe
 
 	memset(tbuf, 0, 64);
 	STATS_TIMEVAL(&tv_start);
-	err = usb_control_transfer(cgpu, usbdev->handle, request_type, bRequest,
-				   wValue, wIndex, tbuf, (uint16_t)bufsiz, timeout);
+	err = libusb_control_transfer(usbdev->handle, request_type,
+		bRequest, wValue, wIndex,
+		tbuf, (uint16_t)bufsiz, timeout);
 	STATS_TIMEVAL(&tv_finish);
 	USB_STATS(cgpu, &tv_start, &tv_finish, err, MODE_CTRL_READ, cmd, SEQ0, timeout);
 	memcpy(buf, tbuf, bufsiz);
