3e331ae44dba949981eac4662fab15b54f640202
MihaiS <Mihai_spatariu@yahoo.com>
2018-06-18 17:42:08 +0300

    Update api.c

--- api.c
+++ api.c
@@ -28,11 +28,19 @@
 #include <limits.h>
 #include <sys/stat.h>
 #include <sys/resource.h>
+#include <libgen.h>
 
+#include "sha2.h"
+#include "compat.h"
+#include "bench_block.h"
 #include "compat.h"
 #include "miner.h"
 #include "util.h"
 #include "klist.h"
+
+#ifdef USE_BITMAIN_C5
+#include "driver-btm-c5.h"
+#endif
 
 #if defined(USE_BFLSC) || defined(USE_AVALON) || defined(USE_AVALON2) || defined(USE_AVALON4) || \
   defined(USE_HASHFAST) || defined(USE_BITFURY) || defined(USE_BLOCKERUPTER) || defined(USE_KLONDIKE) || \
